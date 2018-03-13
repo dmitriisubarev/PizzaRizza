da6955270919041492127995e7ae850e73a00230
dmitriisubarev <dmitriisubarev@gmail.com>
2018-03-13 16:57:28 +0300

    tab меняют размер иконки от активности

--- app/src/main/java/ru/efirus/app/pizzarizza/MainActivity.java
+++ app/src/main/java/ru/efirus/app/pizzarizza/MainActivity.java
@@ -2,8 +2,12 @@ package ru.efirus.app.pizzarizza;
 
 import android.app.ActivityGroup;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TabHost;
+import android.widget.TabWidget;
+import android.widget.TextView;
 
 import com.google.firebase.FirebaseApp;
 
@@ -22,22 +26,22 @@ public class MainActivity extends ActivityGroup {
 
         //добавляем вкладки и назначем отображение активити
         tabSpec = tabHost.newTabSpec("tag1");
-        tabSpec.setIndicator(getString(R.string.tab1));
+        tabSpec.setIndicator("", getResources().getDrawable(R.drawable.tab1_icon_selector));
         tabSpec.setContent(new Intent(this, OneActivity.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag2");
-        tabSpec.setIndicator(getString(R.string.tab2));
+        tabSpec.setIndicator("", getResources().getDrawable(R.drawable.tab2_icon_selector));
         tabSpec.setContent(new Intent(this, TwoActivity.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag3");
-        tabSpec.setIndicator(getString(R.string.tab3));
+        tabSpec.setIndicator("", getResources().getDrawable(R.drawable.tab3_icon_selector));
         tabSpec.setContent(new Intent(this, ThreeActivity.class));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("tag4");
-        tabSpec.setIndicator(getString(R.string.tab4));
+        tabSpec.setIndicator("", getResources().getDrawable(R.drawable.tab4_icon_selector));
         tabSpec.setContent(new Intent(this, FourActivity.class));
         tabHost.addTab(tabSpec);
     }
